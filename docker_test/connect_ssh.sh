4922ed722524859f66f9caa2eacace65c171f00b
Aleksandr Vinogradov <vinogradov.alek@gmail.com>
2023-12-18 13:53:30 +0700

    docker test container scripts

---
+++ docker_test/connect_ssh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sshpass -p password ssh -o StrictHostKeyChecking=no -p 2222 user@localhost
---
+++ docker_test/docker_run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d -p 127.0.0.1:2222:22 --name ubuntu_host_inst ubuntu_host
